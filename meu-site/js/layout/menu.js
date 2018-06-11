6c35ded8aad77ad2b407aa3dc3b04270d078b99f
Nicolas Guilherme <nicolas.gmop@gmail.com>
2018-06-11 15:49:41 -0300

    Incluindo Interatividade no menu + alguns ajustes

--- meu-site/js/layout/menu.js
+++ meu-site/js/layout/menu.js
@@ -1,15 +1,20 @@
-var tamanhoTela = 1024;
+var tela_1024 = 1024;
+var tela_1023 = 1023;
 
 function menuInterativo(){
+    interandoComMenu();
     configuraMenuOnResize();
     configuraMenuOnLoad();
 }
 
-function verificaTamanhoTela(tamanho){
-    let result = $(document).width() >= tamanho ? true : false;
+function tamanhoTelaMaiorQue(tamanhoMaior){
+    let result = $(document).width() >= tamanhoMaior ? true : false;
     return result;
 }
-
+function tamanhoTelaMenorQue(tamanhoMenor){
+    let result = $(document).width() <= tamanhoMenor ? true : false;
+    return result;
+}
 function adicionaClasse(seletor, classe){
     $(seletor).addClass(classe);
 }
@@ -17,7 +22,7 @@ function removeClasse(seletor, classe){
     $(seletor).removeClass(classe);
 }
 function configuraMenuOnLoad(){
-    if(verificaTamanhoTela(tamanhoTela)){
+    if(tamanhoTelaMaiorQue(tela_1024)){
             adicionaClasse($(".navbar"),"container");
             removeClasse($(".navbar-brand"),"border-right");
             removeClasse($(".menu-header"),"fixed-bottom");
@@ -35,7 +40,7 @@ function configuraMenuOnLoad(){
 }
 function configuraMenuOnResize(){
     $(window).resize(function(){
-       if(verificaTamanhoTela(tamanhoTela)){
+       if(tamanhoTelaMaiorQue(tela_1024)){
             adicionaClasse($(".navbar"),"container");
             removeClasse($(".navbar-brand"),"border-right");
             removeClasse($(".menu-header"),"fixed-bottom");
@@ -50,5 +55,39 @@ function configuraMenuOnResize(){
             adicionaClasse($(".menu-header"),"border-top");
             removeClasse($(".menu-header"),"border-bottom");
        }
+    });
+}
+function interandoComMenu(){
+    verificaSeLiFoiClicada();
+}
+function verificaSeLiFoiClicada(){
+    if(tamanhoTelaMenorQue(tela_1023)){
+        let selected = $(".navbar-nav").find(".selected");
+        let listaLi = $(".navbar-nav").find(".nav-item");
+        $(".menu-header .nav-item").click(function(){
+            let self = $(this);
+            exibeNomeIcone(self,listaLi, selected);
+        });
+    }
+}
+function exibeNomeIcone(self,listaLi, selected){
+    if(!self.hasClass("selected")){
+        let selectAnterior = self.parent().find(".selected");
+        selectAnterior.removeClass("selected");
+        selectAnterior.find(".nav-link").removeClass("d-block");
+        selectAnterior.find(".nav-link").addClass("d-none");
+        removeClasse(self.find(".nav-link"),"d-none");
+        adicionaClasse(self, "selected");
+    }
+}
+function hideMenuOnScroll(){
+    let scrollInicial = 0;
+    $(document).scroll(function(){
+        if($(this).scrollTop() > scrollInicial){
+            $(".menu-header").addClass("hide-menu");
+        }else{
+            $(".menu-header").removeClass("hide-menu");
+        }
+        scrollInicial = $(this).scrollTop();
     });
 }
