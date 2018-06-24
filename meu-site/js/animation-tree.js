2942a50a2cd272dfb4dcbf46feb6e541b7bc32f7
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2018-06-24 20:31:35 -0300

    [INCLUSAO] de Logos / animação com js / estilo da arvore de habilidades

---
+++ meu-site/js/animation-tree.js
@@ -0,0 +1,46 @@
+let nodeLeft = $('.section-skill-tree .node-left');
+let nodeRight = $('.section-skill-tree .node-right');
+let log = $('.section-skill-tree .log');
+let seeLeft = $('.see-left');
+let seeRight = $('.see-right');
+
+
+
+    
+nodeLeft.animate({   
+    opacity: .3
+},500);
+
+nodeLeft.animate({   
+    borderBottomWidth: "2px",
+    opacity: 1
+},500);
+
+setTimeout(function(){
+    
+    seeLeft.animate({   
+        opacity: 1
+    },1000);
+    
+}, 1000);
+
+setTimeout(function(){
+    
+    nodeRight.animate({
+        opacity: .3
+    },500);
+    
+    nodeRight.animate({   
+        borderBottomWidth: "2px",
+        opacity: 1
+    },500);
+
+    setTimeout(function(){
+
+        seeRight.animate({   
+            opacity: 1
+        },1000);
+
+    }, 1000);
+    
+}, 2000);    
