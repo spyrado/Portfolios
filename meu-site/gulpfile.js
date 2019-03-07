2653c32b8b231e1963396b6b848ce790f3c69a84
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-03-07 00:14:18 -0300

    [AUTOMATIZANDO] o watch do SASS

--- meu-site/gulpfile.js
+++ meu-site/gulpfile.js
@@ -1,1 +1,16 @@
-var gulp = require('gulp');
+'use strict';
+
+var gulp = require('gulp')
+  ,sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+});
