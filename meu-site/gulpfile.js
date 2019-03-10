5f8569e515587fa2e2beca291765ceaca9693985
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-03-10 18:20:18 -0300

    [UPDATE] Adicionando umas brincadeirinhas ao gulp ;)

--- meu-site/gulpfile.js
+++ meu-site/gulpfile.js
@@ -1,16 +1,42 @@
 'use strict';
 
-var gulp = require('gulp')
-  ,sass = require('gulp-sass');
- 
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  imagemin = require('gulp-imagemin'),
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync').create();
+
 sass.compiler = require('node-sass');
- 
+
+//Minifica as imagens
+gulp.task('imagemin', () =>
+  gulp.src('src/img/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('compress-js', function () {
+  gulp.src('src/js/**/*.js')
+    .pipe(uglify())
+  gulp.dest('dist/js')
+});
+
+// Compila o sass e fica assistindo
 gulp.task('sass', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('dist/css'));
 });
- 
+
 gulp.task('sass:watch', function () {
   gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
 });
